1503298294e6c78c72927cc403278c51ae897559
Anthony Lapham <lapham.2@wright.edu>
2018-04-23 10:49:31 -0400

    text balloon

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-  $("#bdayQ").hover(function() {
-      alert(Providing your birthday helps make sure you get the right Facebook experience
-        for your age.If you want to change who sees this, go to the About section of your profile.For more details, please visit our Data Policy.);
-    },
-    function() {
-      this.html($(""));
-    });
-})
+  $('#bdayQ').balloon({
+    postion: "left"
+  } {
+    html: true,
+    contents: 'Providing your birthday helps make sure you get the right Facebook experience for your age. If you want to change who sees this, go to the About section of your profile. For more details, please visit our Data Policy.'
+  });
+});
